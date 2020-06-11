f72131d948e2e4fb4ce8d6c1ea581fb19f7c3f66
kshitij <anekix001@gmail.com>
2020-06-11 16:05:18 +0530

    problem 1 , filter, mod , zero, range

--- 1.clj
+++ 1.clj
@@ -1,5 +1,1 @@
-(defn problem-1
-    [n]
-    (reduce + (filter #(or (zero? (mod % 3)) (zero? (mod % 5)) ) (range 10000)))
-)
-(problem-1 1000)
+(reduce + (filter #(or (zero? (mod % 3)) (zero? (mod % 5)) ) (range 1000)))
